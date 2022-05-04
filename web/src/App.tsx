e55241957299272c457990091534a1a0440e5f19
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-04 02:05:35 -0300

    Creating Widget

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
+import { Widget } from "./components/Widget"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  return <Widget />
 }
 
 export default App
