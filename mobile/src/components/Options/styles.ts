1a23687488748293297ca1aed29f0383e300d484
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-08 17:52:45 -0300

    Create form

---
+++ mobile/src/components/Options/styles.ts
@@ -0,0 +1,21 @@
+import { theme } from './../../theme/index';
+import { Rows } from 'phosphor-react-native';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  options: {
+    width: '100%',
+    marginBottom: 48,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 32,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_primary
+  }
+});
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -6,6 +6,12 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
+import { Form } from '../Form';
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
     const bottomSheetRef = useRef<BottomSheet>(null)
@@ -33,6 +39,10 @@ function Widget() {
                 backgroundStyle={styles.modal}
                 handleIndicatorStyle={styles.indicator}
             >
+
+                <Form
+                    feedbackType='BUG'
+                />
 
             </BottomSheet>
         </>
