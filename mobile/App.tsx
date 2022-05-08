44e2ad3aa160a3570d07e731c7ad2e62e7458f83
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-08 12:18:32 -0300

    Install Google Fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { Widget } from './src/components/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium
+  });
+
   return (
     <View style={{
       flex: 1,
