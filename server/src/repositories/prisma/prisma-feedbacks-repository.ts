c7897c2332e0148053f25b964141f62dbb5f7b40
Gislaine Costa Pereira <gislaine_costa@hotmail.com>
2022-05-07 21:47:40 -0300

    Finally Dependency inversion

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -2,7 +2,7 @@ import { prisma } from "../../prisma";
 import { FeedBacksRepository, FeedBackCreateData } from "../feedbacks-repository";
 
 
-export class PrismaFeedBackRepository implements FeedBacksRepository {
+  export class PrismaFeedBackRepository implements FeedBacksRepository {
     async create({type, comment, screenshot}: FeedBackCreateData) {
         await prisma.feedback.create({
           data: {
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import nodemailer from "nodemailer";
-import { prisma } from './prisma'
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { PrismaFeedBackRepository } from './repositories/prisma/prisma-feedbacks-repository'
 
 
 export const routes = express.Router()
@@ -17,19 +18,29 @@ var transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = 
+  const prismaFeedBackRepository = new PrismaFeedBackRepository()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedBackRepository
+  );
+  
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot
+  })
 
-  await transport.sendMail({
-    from: "Equipe Dev <quipedev@dev.com.br>",
-    to: "Gislaine Costa <devgislainecosta@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
-      `<p>Tipo do Feedback: ${type} </p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
 
-  return res.status(201).json({ data: feedback });
+  // await transport.sendMail({
+  //   from: "Equipe Dev <quipedev@dev.com.br>",
+  //   to: "Gislaine Costa <devgislainecosta@gmail.com>",
+  //   subject: "Novo feedback",
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+  //     `<p>Tipo do Feedback: ${type} </p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`,
+  //   ].join("\n"),
+  // });
+
+  return res.status(201).send();
 });
